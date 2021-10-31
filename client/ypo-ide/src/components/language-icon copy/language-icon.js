4edcda189b48f8c5f240121f53b12c7a7f9e9507
Or Linzer <orlinzer@gmail.com>
2021-10-31 18:02:24 +0200

    fix bug in attribute

--- client/ypo-ide/src/components/language-icon copy/language-icon.js
+++ client/ypo-ide/src/components/language-icon copy/language-icon.js
@@ -30,18 +30,12 @@ export class LanguageIcon extends React.Component {
           </mask>
           <circle cx="127.5" cy="127.5" r="96" fill="black" mask="url(#language-icon-inner-circle)"/>
 
-          <line x1="47.5" y1="95.5" x2="207.5" y2="95.5" stroke-linecap="round" stroke-width="16" stroke="black" fill="transparent"/>
-          <line x1="47.5" y1="159.5" x2="207.5" y2="159.5" stroke-linecap="round" stroke-width="16" stroke="black" fill="transparent"/>
+          <line x1="47.5" y1="95.5" x2="207.5" y2="95.5" strokeLinecap="round" strokeWidth="16" stroke="black" fill="transparent"/>
+          <line x1="47.5" y1="159.5" x2="207.5" y2="159.5" strokeLinecap="round" strokeWidth="16" stroke="black" fill="transparent"/>
 
-          <path className="a" d="M 127.5 40.5 A 64 64 0 0 0 127.5 214.5" stroke-linecap="round" stroke-width="16" stroke="black" fill="transparent"/>
-          <path className="b" d="M 127.5 40.5 A 64 64 0 0 0 127.5 214.5" stroke-linecap="round" stroke-width="16" stroke="black" fill="transparent"/>
-          <path className="c" d="M 127.5 40.5 A 64 64 0 0 0 127.5 214.5" stroke-linecap="round" stroke-width="16" stroke="black" fill="transparent"/>
-
-          {/* <path d="M 127.5 40.5 A 96 96 0 0 0 127.5 214.5" stroke-linecap="round" stroke-width="16" stroke="black" fill="transparent"/> */}
-          {/* <path className="a" d="M 127.5 40.5 A 127 127 0 0 0 127.5 214.5" stroke-linecap="round" stroke-width="16" stroke="black" fill="transparent"/> */}
-          {/* <path d="M 127.5 40.5 A 127 127 0 0 1 127.5 214.5" stroke-linecap="round" stroke-width="16" stroke="black" fill="transparent"/> */}
-          {/* <path d="M 127.5 40.5 Q 63.5 127.5 127.5 214.5" stroke-linecap="round" stroke-width="16" stroke="black" fill="transparent"/>
-          <path d="M 127.5 40.5 Q 191.5 127.5 127.5 214.5" stroke-linecap="round" stroke-width="16" stroke="black" fill="transparent"/> */}
+          <path className="a" d="M 127.5 40.5 A 64 64 0 0 0 127.5 214.5" strokeLinecap="round" strokeWidth="16" stroke="black" fill="transparent"/>
+          <path className="b" d="M 127.5 40.5 A 64 64 0 0 0 127.5 214.5" strokeLinecap="round" strokeWidth="16" stroke="black" fill="transparent"/>
+          <path className="c" d="M 127.5 40.5 A 64 64 0 0 0 127.5 214.5" strokeLinecap="round" strokeWidth="16" stroke="black" fill="transparent"/>
         </svg>
       </div>
     );
