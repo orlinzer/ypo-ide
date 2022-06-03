f09756f0bd646302f7d663585da75f65a4f9ff03
Or Linzer <orlinzer@gmail.com>
2022-06-03 12:47:03 +0300

    clean dialog comment

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -59,7 +59,6 @@ export const AuthPage: NextPage<AuthPageProps> = ({
             csrfToken={csrfToken}
           />
         </Card>
-        {/* </Dialog> */}
       </Paper>
 
       {/* Error Message */}
